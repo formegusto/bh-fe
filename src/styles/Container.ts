a7b4294093dddf15da78361003708fb03f800bf7
formegusto <formegusto@gmail.com>
2021-12-14 10:01:03 +0900

    styles config

---
+++ src/styles/Container.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const ContainerWidth1240 = styled.div`
+  width: 1240px;
+`;
---
+++ src/styles/Palette.ts
@@ -0,0 +1,21 @@
+export const BLUE = [
+  "#2C5E9E", // main blue
+
+  // getting brighter
+  "#8199B6",
+  "#56749D",
+  "#588DDA",
+  "#5EB4ED",
+  "#9EC8FA",
+];
+
+export const ACHROMATIC = [
+  // getting darker
+  "#333333",
+  "#666666",
+  "#999999",
+  "#AAAAAA",
+  "#CCCCCC",
+  "#EEEEEE",
+  "#FFFFFF",
+];
