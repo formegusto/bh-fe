9bd19818211edf05f6b4c2fe25ee70a10a15c733
formegusto <formegusto@gmail.com>
2021-12-19 15:02:43 +0900

    header okay

--- src/components/common/HeaderComponent.tsx
+++ src/components/common/HeaderComponent.tsx
@@ -18,7 +18,13 @@ function HeaderComponent() {
           <Nav.Main.Block>
             <Nav.Main.Item>About Us</Nav.Main.Item>
             <Nav.Main.Item>Data Information</Nav.Main.Item>
-            <Nav.Main.Item>Data Sharing</Nav.Main.Item>
+            <Nav.Main.Item>
+              Data Sharing
+              <Nav.Main.DropDown>
+                <Nav.Main.DropDownItem>Document</Nav.Main.DropDownItem>
+                <Nav.Main.DropDownItem>Console</Nav.Main.DropDownItem>
+              </Nav.Main.DropDown>
+            </Nav.Main.Item>
             <Nav.Main.Item>Login</Nav.Main.Item>
           </Nav.Main.Block>
         </Nav.Wrap>
@@ -80,15 +86,18 @@ const Nav = {
       color: ${ACHROMATIC[11]};
       box-sizing: border-box;
 
+      & > li:last-child {
+        padding: 0 8px 0 7px;
+      }
+
       & > li:not(:last-child) {
-        padding: 0 8px 0;
         border-right: 1px solid ${ACHROMATIC[11]};
       }
     `,
     Item: styled.li`
       height: 12px;
       cursor: pointer;
-      padding: 0 0 0 7px;
+      padding: 0 8px 0;
     `,
   },
   Main: {
@@ -98,11 +107,13 @@ const Nav = {
       display: flex;
 
       & > li:not(:last-child) {
-        margin: 0 50px 0 0;
+        margin: 0 34px 0 0;
       }
     `,
     Item: styled.li`
-      padding: 12px 0 30px;
+      position: relative;
+
+      padding: 12px 8px 30px;
       box-sizing: border-box;
 
       font-style: normal;
@@ -111,6 +122,73 @@ const Nav = {
       line-height: 16px;
 
       color: ${BLUE[1]};
+
+      cursor: pointer;
+
+      &::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 0;
+
+        width: 100%;
+        height: 3px;
+
+        background-color: ${BLUE[0]};
+        transform: scaleX(0);
+        transition: 0.3s;
+        transform-origin: 0 0;
+      }
+
+      &:hover {
+        color: rgba(38, 68, 109, 0.7);
+
+        &::after {
+          transform: scaleX(1);
+        }
+
+        & > ul {
+          transform: scaleY(1);
+
+          & > li {
+            color: ${ACHROMATIC[15]};
+          }
+        }
+      }
+    `,
+    DropDown: styled.ul`
+      position: absolute;
+      top: 100%;
+      right: 0;
+
+      padding: 10px 10px 20px 150px;
+
+      box-sizing: border-box;
+      background: rgba(38, 68, 109, 0.7);
+
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+
+      transform: scaleY(0);
+      transition: 0.3s;
+      transform-origin: 0 0;
+    `,
+    DropDownItem: styled.li`
+      font-style: normal;
+      font-weight: bold;
+      font-size: 14px;
+      line-height: 16px;
+
+      padding: 4px 10px;
+
+      color: transparent;
+
+      text-shadow: 0px 0px 20px rgba(0, 0, 0, 0.9);
+
+      &:hover {
+        color: ${BLUE[5]} !important;
+      }
     `,
   },
 };
